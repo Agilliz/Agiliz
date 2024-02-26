9ed74cb3e50a1289f2b257c1a24bc8caaa147043
Jonnas <jonnas.oliveira@sptech.school>
2024-02-25 22:32:46 -0300

    Implementação da heurísitica K-opt no algoritmo de geração de rotas - @JonasKauan

--- projetoAgiliz/src/main/java/agiliz/projetoAgiliz/models/CalculadoraRotas.java
+++ projetoAgiliz/src/main/java/agiliz/projetoAgiliz/models/CalculadoraRotas.java
@@ -7,20 +7,39 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-
 public class CalculadoraRotas {
     private Set<String> enderecos;
     private Map<String, Map<String, Double>> mapaDeDistancia;
-    private Map<String, List<String>> adjMap;
-
+    
     public CalculadoraRotas(List<Endereco> enderecos) {
         this.enderecos = new HashSet<>(enderecos.stream().map(Endereco::getId).toList());
         this.mapaDeDistancia = calcularDistancias(enderecos);
-        this.adjMap = definirAdjacencia(enderecos);
+        definirAdjacencia(enderecos);
     }
 
     public List<String> gerarRota(String vtxInicial, String vtxFinal) {
-        return calcularRota(enderecos, vtxInicial, vtxFinal);
+        List<String> rota = calcularRota(enderecos, vtxInicial, vtxFinal);
+        return kOpt(rota);
+    }
+
+    private List<String> kOpt(List<String> rota) {
+        double menorDistancia = calcularDistancia(rota);
+
+        for (int i = 1; i < rota.size() - 2; i++) {
+            List<String> rotaAux = new ArrayList<>(rota);
+            String vtxI = rotaAux.get(i);
+            rotaAux.set(i, rotaAux.get(i + 1));
+            rotaAux.set(i + 1, vtxI);
+            double distancia = calcularDistancia(rotaAux);
+
+
+            if (distancia < menorDistancia) {
+                menorDistancia = distancia;
+                rota = new ArrayList<>(rotaAux);
+            }
+        }
+
+        return rota;
     }
 
     private List<String> calcularRota(Set<String> vtxNaoVisitados, String inicio, String fim) {
@@ -42,7 +61,7 @@ public class CalculadoraRotas {
                 double dIk = mapaDeDistancia.get(rota.get(i)).get(rota.get(k));
                 double impacto = dIj + dJk - dIk;
 
-                if(impacto < impactoMinimo){
+                if (impacto < impactoMinimo) {
                     impactoMinimo = impacto;
                     indiceInsercao = k;
                 }
@@ -64,14 +83,15 @@ public class CalculadoraRotas {
             String vizinhoProximo = null;
 
             for (String vizinho : possiveisNos) {
-                if(vizinho.equals(vtx)) continue;
+                if (vizinho.equals(vtx))
+                    continue;
 
                 if (mapaDeDistancia.get(vtx).get(vizinho) < menorDistancia) {
                     menorDistancia = mapaDeDistancia.get(vtx).get(vizinho);
                     vizinhoProximo = vizinho;
                 }
             }
-            
+
             if (menorDistancia > maiorDistancia) {
                 maiorDistancia = menorDistancia;
                 vizinhoDistante = vizinhoProximo;
@@ -95,16 +115,14 @@ public class CalculadoraRotas {
         return vtxMaisDistante;
     }
 
-    private double calcularDistancia(int indiceAtual, List<String> enderecos, double distanciaTotal) {
-        if (indiceAtual + 1 >= enderecos.size())
-            return distanciaTotal;
+    private double calcularDistancia(List<String> rota) {
+        double distanciaTotal = 0.;
 
-        String origem = enderecos.get(indiceAtual);
-        String destino = enderecos.get(indiceAtual + 1);
+        for (int i = 0; i < rota.size() - 1; i++) {
+            distanciaTotal += mapaDeDistancia.get(rota.get(i)).get(rota.get(i + 1));
+        }
 
-        distanciaTotal += mapaDeDistancia.get(origem).get(destino);
-
-        return calcularDistancia(indiceAtual + 1, enderecos, distanciaTotal);
+        return distanciaTotal;
     }
 
     private static Map<String, List<String>> definirAdjacencia(List<Endereco> enderecos) {
