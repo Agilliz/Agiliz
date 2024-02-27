0a484e57ba282db52ffe6a93a32f5e0830f74716
Uglúk <pedro.silva867@sptech.school>
2024-02-27 11:18:38 -0300

    Adicionando senha padrao de requisição  - @PedroHSilva1009

--- projetoAgiliz/src/main/java/agiliz/projetoAgiliz/models/FornecedorModel.java
+++ projetoAgiliz/src/main/java/agiliz/projetoAgiliz/models/FornecedorModel.java
@@ -10,17 +10,15 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
-@Entity
-@Table(name = "fornecedor")
 @Getter
 @Setter
+@Entity
+@Table(name = "fornecedor")
 public class FornecedorModel implements Serializable {
     private static final long serialVersionUID = 1L;
 
